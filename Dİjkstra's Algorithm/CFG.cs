3a4bf9e48feb8c67a6474441a2071a83e67f442e
Bahadır Tuna Aydın <aydinbahadirtuna@gmail.com>
2023-01-15 02:43:40 +0300

    algorithm done

---
+++ Dİjkstra's Algorithm/CFG.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public class CFG
+{
+    static int V = 9;
+
+    int minDistance(int[] dist, bool[] sptSet)
+    {
+        int min = int.MaxValue;
+        int min_index = -1;
+
+        for (int v = 0; v < V; v++)
+        {
+            if (sptSet[v] == false && dist[v] <= min)
+            {
+                min = dist[v];
+                min_index = v;
+            }
+        }
+        return min_index;
+    }
+
+    void printSolution(int[] dist, int target)
+    {
+        Console.WriteLine("Vertex \t\t Distance " + "from Source\n");
+
+        for (int i = 0; i < V; i++)
+        {
+
+            if (i == target)
+            {
+                Console.WriteLine("The target: " + i + " \t\t " + "Shortest Distance: " + dist[i] + "\n");
+                continue;
+            }
+
+            Console.WriteLine(i + " \t\t " + dist[i] + "\n");
+        }
+
+    }
+
+    public void dijkstra(int[,] graph, int src, int targetV)
+    {
+        int[] dist = new int[V];
+
+        bool[] sptSet = new bool[V];
+
+        for (int i = 0; i < V; i++)
+        {
+            dist[i] = int.MaxValue;
+            sptSet[i] = false;
+        }
+
+        dist[src] = 0;
+
+        for (int count = 0; count < V - 1; count++)
+        {
+            int u = minDistance(dist, sptSet);
+
+            sptSet[u] = true;
+
+            for (int v = 0; v < V; v++)
+            {
+                if (!sptSet[v] && graph[u, v] != 0
+                && dist[u] != int.MaxValue
+                && dist[u] + graph[u, v] < dist[v])
+                {
+                    dist[v] = dist[u] + graph[u, v];
+                }
+            }
+        }
+
+        printSolution(dist, targetV);
+    }
+}
+
+
---
+++ Dİjkstra's Algorithm/Program.cs
@@ -0,0 +1,24 @@
+﻿int[,] graph = new int[,]
+{
+    { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+    { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+    { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+    { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+    { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+    { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+    { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+    { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+    { 0, 0, 2, 0, 0, 0, 6, 7, 0 }
+};
+
+CFG t = new CFG();
+
+Console.Write("Target Vertex: ");
+
+int targetV = Convert.ToInt32(Console.ReadLine());
+
+t.dijkstra(graph, 0, targetV);
+
+
+Console.ReadKey();
+
